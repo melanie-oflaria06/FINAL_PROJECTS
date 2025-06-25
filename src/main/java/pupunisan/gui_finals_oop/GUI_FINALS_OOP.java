3414e0bda316a213409bd9cb3b9b0697cfefef85
Admin <Admin@DESKTOP-EQ55Q8H>
2025-06-25 11:04:01 +0800

    First Commit

---
+++ src/main/java/pupunisan/gui_finals_oop/GUI_FINALS_OOP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pupunisan.gui_finals_oop;
+
+/**
+ *
+ * @author Admin
+ */
+public class GUI_FINALS_OOP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
